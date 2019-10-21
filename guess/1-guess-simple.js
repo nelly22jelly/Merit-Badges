826230ddfb0eefe415a663923a1ea98e24a31618
nelly22jelly <54910642+nelly22jelly@users.noreply.github.com>
2019-10-21 14:17:39 -0400

    First commit

---
+++ guess/1-guess-simple.js
@@ -0,0 +1,22 @@
+/* Guess Game: Random Integer-Driven JavaScript While Loop
+ * Format: while (boolean expression) {} -- indent block code 
+ */
+
+// Declare var answer as random integer between 1 and 100.
+
+// alert the answer while testing.
+
+// Declare var guess, set to 0.
+
+// Define while loop that runs while guess is not equal to answer
+
+  	// Prompt user for guess.
+
+  	/* Add conditionals that give feedback on values:  if / else if */
+  	// If guess less than answer, say too low. 
+
+  	// If guess more than answer, say too high. 
+
+  // Close While Loop Brackets
+
+// Alert that they got it. 
